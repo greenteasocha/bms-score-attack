f561b31012be099cbabf9b00f57a3f80eae7e3d9
k_abe <k_abe@yumemi.co.jp>
2020-05-11 11:47:23 +0900

    .envを反映

--- src/app/Http/Controllers/ContestPageController.php
+++ src/app/Http/Controllers/ContestPageController.php
@@ -5,21 +5,51 @@ use Illuminate\Support\Facades\Log;
 use Illuminate\Http\Request;
 use App\Models\Music;
 use App\Models\Contest;
+use App\Models\Score;
+use Illuminate\Support\Facades\Auth;
+use App\Services\ContestService;
 
 class ContestPageController extends Controller
 {
+    protected $ContestService;
+    public function __construct(ContestService $contest_service)
+    {
+        $this->contestService = $contest_service;
+    }
+
+    public function showAllContests(){
+        return $this->contestService->showAllContests();
+    }
+
     public function aggregateRankingData($contestId){
-        Log::debug($contestId);
-        $music = Contest::where('contestId', $contestId)->first();
-        $scores = $music->scores;
-        
-        return ['scores' => $scores];
 
-        // return $music->music->musicName;
+        $contestPageData = $this->contestService->getContestData($contestId);
 
+        return view("rankings", $contestPageData);
+    }
+
+    public function handlePostedScore(Request $request, $contestId){
+        $contestPageData = $this->contestService->postScore($request, $contestId);
+        // // (存在すれば)そのプレイヤーが過去に提出したハイスコアをチェック
+        // $previousScore = Score::where('contestId', $contestId)->where('userId', $request['userId'])->first();
         
-        // Log::debug($request['userName']);
-        // Log::debug($request['score']);
-        // Log::debug($request['comment']);
+        // if (is_null($previousScore)) {
+        //     // 初提出の場合、レコードを作成
+        //     $score = new Score();
+        //     $score->fill([
+        //         'userId' => $request["userId"],
+        //         'contestId' => $contestId,
+        //         'score' => $request["score"],
+        //         'comment' => $request["comment"],
+        //     ])->save();
+        //     return redirect('/contests/' . $contestId);
+        // } elseif ($request['score'] > $previousScore['score']) {
+        //     $previousScore->score = $request['score'];
+        //     $previousScore->comment = $request['comment'];
+        //     $previousScore->save();
+        //     return redirect('/contests/' . $contestId);
+        // } else {
+            return redirect('/contests/' . $contestId);
+        // }
     }
 }
